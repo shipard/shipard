a444e733c5d40d23ae7c8d7618e516c7abca7892
David Šebík <david@sebik.cz>
2021-06-23 17:25:39 +0000

    Oprava výchozích ikon druhů issues ve starých databázích

--- modules/wkf/core/services.php
+++ modules/wkf/core/services.php
@@ -103,6 +103,7 @@ class ModuleServices extends \E10\CLI\ModuleServices
 	public function onAppUpgrade ()
 	{
 		$s [] = ['end' => '2021-12-31', 'sql' => "UPDATE wkf_base_sections SET icon = '' WHERE icon LIKE 'icon-%'"];
+		$s [] = ['end' => '2021-12-31', 'sql' => "UPDATE wkf_base_issuesKinds SET icon = '' WHERE icon LIKE 'icon-%'"];
 		$this->doSqlScripts ($s);
 
 		$this->checkIssuesSystemKinds();
