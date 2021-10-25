0fa64e05a4f4505ee5bc8104940b6bc019d5346d
David Šebík <david@sebik.cz>
2021-10-25 12:06:29 +0200

    Opravy v instalaci - Ubuntu vs Debian

--- install/install-packages.sh
+++ install/install-packages.sh
@@ -2,20 +2,25 @@
 
 apt --assume-yes --quiet update
 apt --assume-yes --quiet upgrade
-apt install --assume-yes --quiet ca-certificates apt-transport-https software-properties-common
+apt install --assume-yes --quiet ca-certificates apt-transport-https software-properties-common wget curl
 
+#
+# detect OS
+#
+. /etc/os-release
 
 #
 # PHP 8
 #
-add-apt-repository --yes ppa:ondrej/php
-apt --assume-yes --quiet update
-apt --assume-yes --quiet upgrade
-
-#Raspberry Pi - raspbian
-#sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-#echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
-#sudo apt update
+if [ "$NAME" = "Ubuntu" ]; then
+    add-apt-repository --yes ppa:ondrej/php
+    apt --assume-yes --quiet update
+    apt --assume-yes --quiet upgrade
+else
+    sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+    echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
+    apt --assume-yes --quiet update
+fi
 
 #
 # NGINX
@@ -84,7 +89,12 @@ pip3 install PyPDF4
 #
 # chromium & puppeteer
 #
-apt install --assume-yes --quiet chromium-browser
+if [ "$NAME" = "Ubuntu" ]; then
+	apt install --assume-yes --quiet chromium-browser
+else
+	apt install --assume-yes --quiet chromium
+fi
+
 npm -g i puppeteer-core
 
 #
