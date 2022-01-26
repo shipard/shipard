a3d4aa4256f47f1608c2430a37acae42d4f3f89c
David Šebík <sebik@shipard.cz>
2022-01-26 21:48:11 +0100

    IoT: oprava více zón videodohledu v mobilní aplikaci

--- modules/mac/vs/StartMenu.php
+++ modules/mac/vs/StartMenu.php
@@ -14,6 +14,7 @@ class StartMenu extends Utility
 	{
 		$tableZones = $this->app->table ('mac.base.zones');
 		$usersZones = $tableZones->usersZones('vs-main');
+		$cnt = 0;
 		foreach ($usersZones as $z)
 		{
 			$tileInfo = [
@@ -21,11 +22,12 @@ class StartMenu extends Utility
 				't1' => $z['sn'], 'class' => 'e10-small',
 				'icon' => 'deviceTypes/camera',
 				'object' => 'widget',
-				'order' => 100000,
+				'order' => 100000 + $cnt,
 				'path' => 'widget/mac.vs.WidgetLive/zone-'.$z['ndx'],
 			];
 
-			$content ['start']['items']['zone-1'] = $tileInfo;
+			$content ['start']['items']['zone-'.$z['ndx']] = $tileInfo;
+			$cnt++;
 		}
 	}
 }
