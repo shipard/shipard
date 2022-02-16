59e1f2f8713b87c3dfb186ba8a5c6d030960d94f
David Šebík <sebik@shipard.cz>
2022-02-16 17:05:50 +0100

    IoT: příjem dat ze senzorů lépe ignoruje nevalidní hodnoty

--- modules/mac/iot/libs/IoTSensorsDataReceiver.php
+++ modules/mac/iot/libs/IoTSensorsDataReceiver.php
@@ -52,8 +52,10 @@ class IoTSensorsDataReceiver extends Utility
 			$rows = $this->db()->query($q);
 			foreach ($rows as $r)
 			{
-				if ($sensorData['value'] === '' || $sensorData['value'] === false)
+				if ($sensorData['value'] === '' || $sensorData['value'] === false || $sensorData['value'] === 'nan')
 					$sensorData['value'] = 0;
+				if (is_string($sensorData['value']))
+					$sensorData['value'] = floatval($sensorData['value']);
 				$this->db()->query('UPDATE [mac_iot_sensorsValues] SET [value] = %f', $sensorData['value'],
 					', [time] = %t', $now, ', [counter] = [counter] + 1',
 					' WHERE [ndx] = %i', $r['ndx']);
