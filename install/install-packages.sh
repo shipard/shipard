3d3f8d677db2c2bdaf6f340a2b5353db80df27d0
David Šebík <sebik@shipard.cz>
2022-05-07 19:38:31 +0200

    Příprava na instalaci pod Ubuntu 22.04

--- install/install-packages.sh
+++ install/install-packages.sh
@@ -5,6 +5,16 @@ apt --assume-yes --quiet upgrade
 apt install --assume-yes --quiet ca-certificates apt-transport-https software-properties-common wget curl
 
 #
+# convert semVer number to int
+#
+function versionToInt() {
+  local IFS=.
+  parts=($1)
+  let val=1000000*parts[0]+1000*parts[1]+parts[2]
+  echo $val
+}
+
+#
 # detect OS
 #
 . /etc/os-release
@@ -13,9 +23,14 @@ apt install --assume-yes --quiet ca-certificates apt-transport-https software-pr
 # PHP 8
 #
 if [ "$NAME" = "Ubuntu" ]; then
-    add-apt-repository --yes ppa:ondrej/php
-    apt --assume-yes --quiet update
-    apt --assume-yes --quiet upgrade
+    currentVersion=$(versionToInt $VERSION_ID)
+    min81Version=$(versionToInt 22.04.0)
+
+    if [ "$currentVersion" -lt "$min81Version" ]; then
+        add-apt-repository --yes ppa:ondrej/php
+        apt --assume-yes --quiet update
+        apt --assume-yes --quiet upgrade	
+    fi
 else
     sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
     echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
