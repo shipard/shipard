607af9cbc9142c22ec331d29ec9c4e59f245734d
David Šebík <sebik@shipard.cz>
2022-10-17 19:02:22 +0200

    Výchozí umístění loga v hlavičce dokladu je vpravo

--- modules/e10doc/core/libs/reports/DocReportBase.php
+++ modules/e10doc/core/libs/reports/DocReportBase.php
@@ -64,7 +64,7 @@ class DocReportBase extends FormReport
 		$this->allProperties = $this->app()->cfgItem('e10.base.properties', []);
 
 		$this->data['options']['docReportsPersonsSigns'] = intval($this->app()->cfgItem ('options.appearanceDocs.docReportsPersonsSigns', 0));
-		$this->data['options']['docReportsHeadLogoRight'] = intval($this->app()->cfgItem ('options.appearanceDocs.docReportsHeadLogoPlace', 0));
+		$this->data['options']['docReportsHeadLogoRight'] = intval($this->app()->cfgItem ('options.appearanceDocs.docReportsHeadLogoPlace', 1));
 		$this->data['options']['docReportsTablesRoundedCorners'] = intval($this->app()->cfgItem ('options.appearanceDocs.docReportsTablesCorners', 1));
 		$this->data['options']['accentColor'] = $this->app()->cfgItem ('options.appearanceDocs.accentColor', '');
 		if ($this->data['options']['accentColor'] === '')
