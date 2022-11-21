d19c76477bde61564aa29113fd07a0e14b71aab0
Anna Šebíková <anna.sebik@gmail.com>
2022-11-21 12:40:18 +0000

    Tisková sestava Přihlášky ZUŠ v moderním stylu

--- modules/e10pro/zus/libs/ReportPrihlaska.php
+++ modules/e10pro/zus/libs/ReportPrihlaska.php
@@ -5,7 +5,7 @@ require_once __SHPD_MODULES_DIR__ . 'e10pro/zus/zus.php';
 use E10Pro\Zus\zusutils, \e10\utils, \e10\str, \E10\FormReport;
 
 
-class ReportPrihlaska extends FormReport
+class ReportPrihlaska extends \e10doc\core\libs\reports\DocReportBase
 {
 	/** @var \e10\persons\TablePersons $tablePersons */
 	var $tablePersons;
@@ -25,6 +25,9 @@ class ReportPrihlaska extends FormReport
 
 	public function loadData ()
 	{
+		parent::loadData();
+		$this->loadData_DocumentOwner ();
+
 		$this->tablePersons = $this->app()->table('e10.persons.persons');
 		$this->allProperties = $this->app()->cfgItem('e10.base.properties', []);
 
@@ -45,56 +48,5 @@ class ReportPrihlaska extends FormReport
 
 
 		$this->data ['webSentDate'] = utils::datef($this->recData['webSentDate'], '%d, %T');
-
-		$this->loadData_DocumentOwner();
-	}
-
-	function loadData_DocumentOwner ()
-	{
-		$this->ownerNdx = intval($this->app()->cfgItem('options.core.ownerPerson', 0));
-		if ($this->ownerNdx)
-		{
-			$this->data ['owner'] = $this->tablePersons->loadItem($this->ownerNdx);
-			$this->data ['owner']['lists'] = $this->tablePersons->loadLists($this->data ['owner']);
-			$this->ownerCountry = FALSE;
-			if (isset($this->data ['owner']['lists']['address'][0]))
-			{
-				$this->data ['owner']['address'] = $this->data ['owner']['lists']['address'][0];
-				$this->ownerCountry = $this->app->cfgItem('e10.base.countries.' . $this->data ['owner']['lists']['address'][0]['country']);
-				$this->data ['owner']['address']['countryName'] = $this->ownerCountry['name'];
-				$this->data ['owner']['address']['countryNameEng'] = $this->ownerCountry['engName'];
-				$this->data ['owner']['address']['countryNameSC2'] = $this->ownerCountry['sc2'];
-			}
-			foreach ($this->data ['owner']['lists']['properties'] as $iii)
-			{
-				if ($iii['group'] == 'ids')
-				{
-					$name = '';
-					if ($iii['property'] == 'taxid')
-					{
-						$name = 'DIČ';
-						$this->data ['owner']['vatId'] = $iii['value'];
-						$this->data ['owner']['vatIdCore'] = substr($iii['value'], 2);
-					}
-					else
-						if ($iii['property'] == 'oid')
-							$name = 'IČ';
-
-					if ($name != '')
-						$this->data ['owner_identifiers'][] = array('name' => $name, 'value' => $iii['value']);
-				}
-				if ($iii['group'] == 'contacts')
-				{
-					$name = $this->allProperties[$iii['property']]['name'];
-					$this->data ['owner_contacts'][] = array('name' => $name, 'value' => $iii['value']);
-				}
-			}
-
-			$ownerAtt = \E10\Base\getAttachments($this->table->app(), 'e10.persons.persons', $this->ownerNdx, TRUE);
-			foreach ($ownerAtt as $oa)
-			{
-				$this->data ['owner']['logo'][$oa['name']] = $oa;
-			}
-		}
 	}
 }
