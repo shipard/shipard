c31cbdff3cd9b96282a256256962a452ab417611
David Šebík <sebik@shipard.cz>
2022-12-01 23:23:09 +0100

    Oprava URL pro helpdesk

--- src/_deprecated/lib/helpdesk/WidgetHelpDeskShipard.php
+++ src/_deprecated/lib/helpdesk/WidgetHelpDeskShipard.php
@@ -11,7 +11,7 @@ class WidgetHelpDeskShipard extends \lib\wkf\WidgetIframe
 	public function init()
 	{
 		$dsId = $this->app->cfgItem('dsid');
-		$helpDeskUrl = 'https://' . 'system.shipard.app';
+		$helpDeskUrl = 'https://' . 'shipard.app';
 		$this->url = $helpDeskUrl."/app/!/widget/viewer/helpdesk.core.tickets/hosting.core.libs.ViewerHelpdeskRemote;dsId:{$dsId}?mainWidgetMode=1&disableAppRightMenu=1&dsId=".$dsId;
 
 		parent::init();
