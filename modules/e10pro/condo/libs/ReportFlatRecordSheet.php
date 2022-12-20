6f2811b77e059e0c24ec791c628d1e0c04e0801e
David Šebík <sebik@shipard.cz>
2022-12-20 11:01:52 +0100

    SVJ: příprava na sestavu Evidenční list

---
+++ modules/e10pro/condo/libs/ReportFlatRecordSheet.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace e10pro\condo\libs;
+
+
+/**
+ * class ReportFlatRecordSheet
+ */
+class ReportFlatRecordSheet extends \e10doc\core\libs\reports\DocReportBase
+{
+	var \e10pro\condo\libs\FlatInfo $flatInfo;
+
+	function init ()
+	{
+		$this->reportId = 'e10pro.condo.flatRecordSheet';
+		$this->reportTemplate = 'reports.modern.e10pro.condo.flatrecordsheet';
+		$this->paperOrientation = 'portrait';
+	}
+
+	public function loadData ()
+	{
+    parent::loadData();
+		$this->loadData_DocumentOwner ();
+
+		$this->data['workOrder'] = $this->app()->loadItem($this->recData['workOrder'], 'e10mnf.core.workOrders');
+		$this->data['person'] = $this->app()->loadItem($this->data['workOrder']['customer'], 'e10.persons.persons');
+
+    $this->flatInfo = new \e10pro\condo\libs\FlatInfo($this->app());
+    $this->flatInfo->setWorkOrder($this->recData['ndx']);
+    $this->flatInfo->loadInfo();
+
+    $contentTitle = ['text' => 'Informace o bytové jednotce', 'class' => 'h3'];
+    foreach ($this->flatInfo->data['vdsContent'] as $cc)
+    {
+      $cc['params'] = ['hideHeader' => 1, ];
+      $cc['title'] = $contentTitle;
+      $this->data['contents'][] = $cc;
+
+      break;
+    }
+
+    if ($this->flatInfo->data['personsList'])
+    {
+      $contentTitlePersons = ['text' => 'Kontaktní údaje', 'class' => 'h3'];
+      $cc = $this->flatInfo->data['personsList'];
+      unset($cc['pane']);
+      $cc['title'] = $contentTitlePersons;
+      $this->data['contents'][] = $cc;
+    }
+
+    if ($this->flatInfo->data['rowsContent'])
+		{
+			$contentTitleAdvances = ['text' => 'Výše měsíčních záloh', 'class' => 'h3'];
+			$cc = $this->flatInfo->data['rowsContent'];
+			unset($cc['pane']);
+			$cc['title'] = $contentTitleAdvances;
+
+			$this->data['contents'][] = $cc;
+		}
+	}
+}
