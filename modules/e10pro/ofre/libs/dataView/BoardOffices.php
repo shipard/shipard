d07a7a2ed17369f626a46b99496d2055579d87a7
David Šebík <sebik@shipard.cz>
2023-02-08 08:33:07 +0100

    Úpravy v dataView `BoardOffices`

--- modules/e10pro/ofre/libs/dataView/BoardOffices.php
+++ modules/e10pro/ofre/libs/dataView/BoardOffices.php
@@ -9,11 +9,39 @@ use \lib\dataView\DataView, \Shipard\Utils\Utils, \Shipard\Utils\Json;
  */
 class BoardOffices extends DataView
 {
+  var $pageNumber = 0;
+  var $cntCols = 0;
+  var $cntRows = 0;
+
+  var $countCells = 0;
+  var $countPages = 0;
+  var $pageSize = 0;
+
+  var $pageBtnSizeX = 0;
+  var $pageBtnSizeY = 0;
+  var $pageBtnSizeXGap = 0;
+
+  var $displayWidth = 2880;
+  var $displayHeight = 2160;
+
 	protected function init()
 	{
 		parent::init();
 
 		$this->checkRequestParamsList('docKinds', TRUE);
+		$this->checkRequestParamsList('withLabels');
+		$this->checkRequestParamsList('withoutLabels');
+
+    $this->cntCols = $this->requestParam ('cntCols', 5);
+    $this->cntRows = $this->requestParam ('cntRows', 10);
+
+    if ($this->app()->testGetParam('pageNumber') !== '')
+      $this->pageNumber = intval($this->app()->testGetParam('pageNumber'));
+
+    $this->pageSize = $this->cntCols * $this->cntRows;
+
+    $this->countCells = $this->countCells();
+    $this->countPages = intval($this->countCells / $this->pageSize) + 1;
 	}
 
 	protected function loadData()
@@ -25,10 +53,27 @@ class BoardOffices extends DataView
 		array_push ($q, ' WHERE 1');
 		array_push ($q, ' AND wo.docStateMain = %i', 1);
 
+		if (isset($this->requestParams['withLabels']) && count($this->requestParams['withLabels']))
+		{
+			array_push ($q, ' AND EXISTS (',
+				'SELECT ndx FROM e10_base_clsf WHERE wo.ndx = recid AND tableId = %s', 'e10mnf.core.workOrders',
+				' AND [clsfItem] IN %in', $this->requestParams['withLabels'],
+				')');
+		}
+		if (isset($this->requestParams['withoutLabels']) && count($this->requestParams['withoutLabels']))
+		{
+			array_push ($q, ' AND NOT EXISTS (',
+				'SELECT ndx FROM e10_base_clsf WHERE wo.ndx = recid AND tableId = %s', 'e10mnf.core.workOrders',
+				' AND [clsfItem] IN %in', $this->requestParams['withoutLabels'],
+				')');
+		}
+
     if (isset($this->requestParams['docKinds']))
       array_push ($q, ' AND wo.docKind IN %in', $this->requestParams['docKinds']);
 		array_push ($q, ' ORDER BY custs.[fullName], custs.[ndx]');
 
+    $sqlLimitStart = $this->pageNumber * $this->pageSize;
+    array_push ($q, ' LIMIT %i', $sqlLimitStart, ', %i', $this->pageSize);
 		$t = [];
 		$pks = [];
 
@@ -68,46 +113,142 @@ class BoardOffices extends DataView
 	protected function renderDataAs($showAs)
 	{
 		//if ($showAs === 'html')
-			return $this->renderDataAsBoardTable();
+			return $this->renderDataAsBoardTable($showAs);
 
 		return parent::renderDataAs($showAs);
 	}
 
-	protected function renderDataAsBoardTable()
+	protected function renderDataAsBoardTable($showAs)
 	{
-    $cntCols = $this->requestParam ('cntCols', 5);
-
     $c = '';
+    $tableCells = '';
 
     $colNum = 0;
-		$c .= "<table style='width: 100%;'>";
-    $c .= "<tr>";
+    $rowNum = 0;
+		$tableCells .= "<table style='width: 100%;' class='bo-offices'>\n";
+
 		foreach ($this->data['table'] as $person)
 		{
-      if ($colNum % $cntCols === 0)
+      if ($colNum % $this->cntCols === 0)
       {
-        $c .= "</tr>";
-        $c .= "<tr data-style='border-bottom: 1px solid rgba(0,0,0,.2);'>";
+        $rowNum++;
+
+        if ($rowNum > $this->cntRows)
+          break;
+
+        if ($colNum)
+        $tableCells .= "</tr>\n";
+
+        $tableCells .= "<tr data-style='border-bottom: 1px solid rgba(0,0,0,.2);'>";
       }
-      $c .= "<td style='vertical-align: top;'>";
+      $tableCells .= "<td id='office-btn-{$person['ndx']}' data-link-url='?officeNdx={$person['ndx']}' data-link-type='1' data-page-number='-1' style='vertical-align: top; width:20%;' class='sc-click-element-pu'>";
 
-      $c .= "<div style=' border-left: 6px solid #AAA; padding: .4rem; margin: 1rem; height: 100%;'>";
+      $tableCells .= "<div style=' border-left: 6px solid #AAA; padding: .4rem; margin: 1rem; height: 100%;'>";
 
-      $c .= '<h5>'.utils::es($person['custName']).'</h5>';
+      $tableCells .= '<h5>'.utils::es($person['custName']).'</h5>';
       if (isset($person['oid']))
-        $c .= "<span class='text-nowrap'>".Utils::es('IČ').' '.Utils::es($person['oid']).'</span> ';
+      $tableCells .= "<span class='text-nowrap'>".Utils::es('IČ').' '.Utils::es($person['oid']).'</span> ';
       if (isset($person['email']))
-        $c .= "<span class='text-nowrap'>".$this->app()->ui()->icon('user/envelope').' '.Utils::es($person['email']).'</span><br>';
+      $tableCells .= "<span class='text-nowrap'>".$this->app()->ui()->icon('user/envelope').' '.Utils::es($person['email']).'</span><br>';
 
-      $c .= "</div>";
-      $c .= "</td>";
+      $tableCells .= "</div>";
+      $tableCells .= "</td>";
 
-        $colNum++;
-
+      $colNum++;
 		}
-    $c .= "</tr>";
-		$c .= '</table>';
+    $tableCells .= "</tr>\n";
+		$tableCells .= "</table>\n";
+
+    $c .= $tableCells;
+
+    $this->data['tableCells'] = $tableCells;
+    $this->data['pageNumber'] = strval($this->pageNumber);
+
+    $this->pageBtnSizeX = 255;
+    $this->pageBtnSizeY = 100;
+    $this->pageBtnSizeXGap = 20;
+
+    $paginationDivOpen = '';
+    $paginationDivOpen .= "<div class='bo-pagination-bar' style=''";
+    $paginationDivOpen .= " id='shp-sc-page-info'";
+    $paginationDivOpen .= " data-this-id='page-btn-{$this->pageNumber}'";
+    $paginationDivOpen .= ">";
+    $this->data['paginationDivOpen'] = $paginationDivOpen;
+
+    $c .= "\n\n".$paginationDivOpen;
+
+    $c .= "<table style='width: 100%;' class='bo-pagination'>\n";
+    $c .= "<tr style='height: {$this->pageBtnSizeY}px;'>\n";
+
+    $c .= "<td style='width: 10%;'>";
+    $c .= "</td>\n";
+
+    $c .= "<td style='width: 80%; vertical-align: middle; text-align: center; padding:0; margin: 0; font-size: 40px;'>";
+    $c .= "<div style='display:flex; align-items: center; justify-content: center;'>";
+
+    $paginationButtons = '';
+    for ($i = 0; $i < $this->countPages; $i++)
+    {
+      $active = ($this->pageNumber === $i) ? ' active' : '';
+      $paginationButtons .= "<div id='page-btn-$i' data-link-url='?pageNumber={$i}' data-link-type='0' data-page-number='{$i}' class='sc-click-element-fs board-office-page-btn$active'>".strval($i + 1).'</div>';
+    }
+
+    $this->data['paginationButtons'] = $paginationButtons;
+    $c .= $paginationButtons;
+    $c .= "</div>\n";
+
+    $c .= "</td>\n";
+
+    $c .= "<td style='width: 10%; text-align: right;'>";
+    $c .= "</td>\n";
+    $c .= "</tr>\n";
+    $c .= "</table>\n";
+    $c .= '</div>';
+
+    $c .= "\n<textarea id='shp-sc-page-info-result' style='display: none;'></textarea>";
+
+    $jsScripts = '';
+    $jsScripts .= "<script type='text/javascript' src='/www-root/templates/web/libs/scCreator.js?v=22'></script>";
+    $this->data['jsScripts'] = $jsScripts;
+    $c .= $jsScripts;
+
+    if ($showAs === 'none')
+      return '';
 
 		return $c;
 	}
+
+  protected function countCells()
+  {
+    $q [] = 'SELECT COUNT(*) AS [cnt]';
+    array_push ($q, ' FROM [e10mnf_core_workOrders] AS wo');
+    array_push ($q, ' LEFT JOIN [e10_persons_persons] AS custs ON wo.customer = custs.ndx');
+    array_push ($q, ' WHERE 1');
+    array_push ($q, ' AND wo.docStateMain = %i', 1);
+
+		if (isset($this->requestParams['withLabels']) && count($this->requestParams['withLabels']))
+		{
+			array_push ($q, ' AND EXISTS (',
+				'SELECT ndx FROM e10_base_clsf WHERE wo.ndx = recid AND tableId = %s', 'e10mnf.core.workOrders',
+				' AND [clsfItem] IN %in', $this->requestParams['withLabels'],
+				')');
+		}
+		if (isset($this->requestParams['withoutLabels']) && count($this->requestParams['withoutLabels']))
+		{
+			array_push ($q, ' AND NOT EXISTS (',
+				'SELECT ndx FROM e10_base_clsf WHERE wo.ndx = recid AND tableId = %s', 'e10mnf.core.workOrders',
+				' AND [clsfItem] IN %in', $this->requestParams['withoutLabels'],
+				')');
+		}
+
+    if (isset($this->requestParams['docKinds']))
+      array_push ($q, ' AND wo.docKind IN %in', $this->requestParams['docKinds']);
+    array_push ($q, ' ORDER BY custs.[fullName], custs.[ndx]');
+
+    $cnt = $this->db()->query($q)->fetch();
+    if ($cnt)
+      return intval($cnt['cnt']);
+
+    return 0;
+  }
 }
