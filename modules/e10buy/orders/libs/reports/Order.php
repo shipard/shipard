80bf262d612323ef1a76a8221a08067397d71acd
Anna Šebíková <anna.sebik@gmail.com>
2023-02-09 13:58:52 +0000

    Tisková sestava vydané objednávky

--- modules/e10buy/orders/libs/reports/Order.php
+++ modules/e10buy/orders/libs/reports/Order.php
@@ -13,9 +13,8 @@ class Order extends DocReportBase
 {
 	function init ()
 	{
-		$this->reportId = 'e10buy.orders.order';
-		$this->reportTemplate = 'e10buy.orders.order';
-		$this->paperOrientation = 'portrait';
+		parent::init();
+		$this->setReportId('e10buy.orders.order');
 	}
 
 	public function loadData ()
