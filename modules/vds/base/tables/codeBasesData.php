2ca40a2570e1c469a134fd64247a9a738cef826f
David Šebík <sebik@shipard.cz>
2023-04-14 11:04:27 +0200

    Pokusy s definicí vlastních číselníků

---
+++ modules/vds/base/tables/codeBasesData.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace vds\base;
+use \Shipard\Utils\Json, \Shipard\Viewer\TableView, \Shipard\Form\TableForm, \Shipard\Table\DbTable;
+
+
+/**
+ * class TableCodeBasesData
+ */
+class TableCodeBasesData extends DbTable
+{
+	public function __construct ($dbmodel)
+	{
+		parent::__construct ($dbmodel);
+		$this->setName ('vds.base.codeBasesData', 'vds_base_codeBasesData', 'Data vlastních číselníků');
+	}
+
+  public function subColumnsInfo ($recData, $columnId)
+	{
+		if ($columnId === 'data')
+		{
+			$codeBaseDef = $this->app()->cfgItem('vds.codeBasesDefs.' . $recData['codeBaseDef'], NULL);
+
+			if (!$codeBaseDef || !isset($codeBaseDef['vds']) || !$codeBaseDef['vds'])
+				return FALSE;
+
+      $vdsDefRecData = $this->app()->loadItem($codeBaseDef['vds'], 'vds.base.defs');
+      if (!$vdsDefRecData)
+        return FALSE;
+
+      $vds = Json::decode($vdsDefRecData['structure']);
+      if (!$vds || !isset($vds['fields']))
+        return FALSE;
+
+			return $vds['fields'];
+
+			return FALSE;
+		}
+
+		return parent::subColumnsInfo ($recData, $columnId);
+	}
+
+	public function createHeader ($recData, $options)
+	{
+		$hdr = parent::createHeader ($recData, $options);
+
+		$hdr ['info'][] = ['class' => 'title', 'value' => $recData ['fullName']];
+
+		return $hdr;
+	}
+}
+
+
+/**
+ * class ViewCodeBasesData
+ */
+class ViewCodeBasesData extends TableView
+{
+	public function init ()
+	{
+		parent::init();
+
+//		$this->objectSubType = TableView::vsDetail;
+//		$this->enableDetailSearch = TRUE;
+
+		$this->setMainQueries ();
+	}
+
+	public function renderRow ($item)
+	{
+		$listItem ['pk'] = $item ['ndx'];
+		$listItem ['icon'] = $this->table->tableIcon ($item);
+		$listItem ['t1'] = $item['fullName'];
+
+		return $listItem;
+	}
+
+	public function selectRows ()
+	{
+		$fts = $this->fullTextSearch ();
+
+		$q [] = 'SELECT cbData.* ';
+		array_push ($q, ' FROM [vds_base_codeBasesData] AS [cbData]');
+		array_push ($q, ' WHERE 1');
+
+		// -- fulltext
+		if ($fts != '')
+		{
+			array_push ($q, ' AND (');
+			array_push ($q, ' cbData.[fullName] LIKE %s', '%'.$fts.'%');
+			array_push ($q, ')');
+		}
+
+		$this->queryMain ($q, '[cbData].', ['[fullName]', '[ndx]']);
+		$this->runQuery ($q);
+	}
+}
+
+
+/**
+ * class FormCodeBaseDataItem
+ */
+class FormCodeBaseDataItem extends TableForm
+{
+	public function renderForm ()
+	{
+		$this->setFlag ('formStyle', 'e10-formStyleSimple');
+		$this->setFlag ('sidebarPos', TableForm::SIDEBAR_POS_RIGHT);
+		$this->setFlag ('maximize', 1);
+
+		$tabs ['tabs'][] = ['text' => 'Základní', 'icon' => 'system/formHeader'];
+		//$tabs ['tabs'][] = ['text' => 'Struktura', 'icon' => 'formStructure'];
+
+		$this->openForm ();
+			$this->openTabs ($tabs);
+				$this->openTab ();
+					$this->addColumnInput ('fullName');
+					$this->addColumnInput ('codeBaseDef');
+          $this->addSubColumns ('data');
+				$this->closeTab();
+        /*
+				$this->openTab (TableForm::ltNone);
+					$this->addInputMemo ('structure', NULL, TableForm::coFullSizeY, DataModel::ctCode);
+				$this->closeTab ();
+        */
+			$this->closeTabs();
+		$this->closeForm ();
+	}
+}
---
+++ modules/vds/base/tables/codeBasesDefs.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace vds\base;
+use \e10\utils, \e10\json, \e10\TableView, \e10\TableForm, \e10\DbTable, \e10,E10\DataModel;
+
+
+/**
+ * class TableCodeBasesDefs
+ */
+class TableCodeBasesDefs extends DbTable
+{
+	public function __construct ($dbmodel)
+	{
+		parent::__construct ($dbmodel);
+		$this->setName ('vds.base.codeBasesDefs', 'vds_base_codeBasesDefs', 'Definice vlastních číselníků');
+	}
+
+	public function createHeader ($recData, $options)
+	{
+		$hdr = parent::createHeader ($recData, $options);
+
+		$hdr ['info'][] = ['class' => 'title', 'value' => $recData ['fullName']];
+
+		return $hdr;
+	}
+
+	public function saveConfig ()
+	{
+		$list = [];
+
+		$rows = $this->app()->db->query ('SELECT * FROM [vds_base_codeBasesDefs] WHERE [docState] != 9800 ORDER BY [ndx]');
+
+		foreach ($rows as $r)
+    {
+			$list [$r['ndx']] =
+      [
+        'ndx' => $r ['ndx'],
+        'fn' => $r ['fullName'],
+        'vds' => $r ['vds']
+      ];
+    }
+
+		// save to file
+		$cfg ['vds']['codeBasesDefs'] = $list;
+		file_put_contents(__APP_DIR__ . '/config/_vds.codeBasesDefs.json', utils::json_lint (json_encode ($cfg)));
+	}
+}
+
+
+/**
+ * class ViewCodeBasesDefs
+ */
+class ViewCodeBasesDefs extends TableView
+{
+	public function init ()
+	{
+		parent::init();
+
+//		$this->objectSubType = TableView::vsDetail;
+//		$this->enableDetailSearch = TRUE;
+
+		$this->setMainQueries ();
+	}
+
+	public function renderRow ($item)
+	{
+		$listItem ['pk'] = $item ['ndx'];
+		$listItem ['icon'] = $this->table->tableIcon ($item);
+		$listItem ['t1'] = $item['fullName'];
+
+		return $listItem;
+	}
+
+	public function selectRows ()
+	{
+		$fts = $this->fullTextSearch ();
+
+		$q [] = 'SELECT cbDefs.* ';
+		array_push ($q, ' FROM [vds_base_codeBasesDefs] AS [cbDefs]');
+		array_push ($q, ' WHERE 1');
+
+		// -- fulltext
+		if ($fts != '')
+		{
+			array_push ($q, ' AND (');
+			array_push ($q, ' cbDefs.[fullName] LIKE %s', '%'.$fts.'%');
+			array_push ($q, ')');
+		}
+
+		$this->queryMain ($q, '[cbDefs].', ['[fullName]', '[ndx]']);
+		$this->runQuery ($q);
+	}
+}
+
+
+/**
+ * class FormCodeBaseDef
+ */
+class FormCodeBaseDef extends TableForm
+{
+	public function renderForm ()
+	{
+		$this->setFlag ('formStyle', 'e10-formStyleSimple');
+		$this->setFlag ('sidebarPos', TableForm::SIDEBAR_POS_RIGHT);
+		$this->setFlag ('maximize', 1);
+
+		$tabs ['tabs'][] = ['text' => 'Základní', 'icon' => 'system/formHeader'];
+		//$tabs ['tabs'][] = ['text' => 'Struktura', 'icon' => 'formStructure'];
+
+		$this->openForm ();
+			$this->openTabs ($tabs);
+				$this->openTab ();
+					$this->addColumnInput ('fullName');
+					$this->addColumnInput ('vds');
+				$this->closeTab();
+        /*
+				$this->openTab (TableForm::ltNone);
+					$this->addInputMemo ('structure', NULL, TableForm::coFullSizeY, DataModel::ctCode);
+				$this->closeTab ();
+        */
+			$this->closeTabs();
+		$this->closeForm ();
+	}
+}
