b9c6b6a146b597377056898475a79c7222e549fe
David Šebík <sebik@shipard.cz>
2023-05-07 08:11:15 +0200

    Příjemky podporují zadávání ceny v řádku cenou celkem

--- modules/e10doc/stockIn/libs/FormStockInRow.php
+++ modules/e10doc/stockIn/libs/FormStockInRow.php
@@ -1,13 +1,18 @@
 <?php
-
 namespace e10doc\stockIn\libs;
 
+use \Shipard\Application\DataModel;
 
+
+/**
+ * class FormStockInRow
+ */
 class FormStockInRow extends \e10doc\core\libs\FormDocRows
 {
 	public function renderForm ()
 	{
 		$ownerRecData = $this->option ('ownerRecData');
+		$testDocRowPriceSource = intval($this->app()->cfgItem('options.experimental.testDocRowPriceSource', 0));
 
 		$this->openForm (self::ltGrid);
 			$this->addColumnInput ('itemType', self::coHidden);
@@ -19,14 +24,39 @@ class FormStockInRow extends \e10doc\core\libs\FormDocRows
 				$this->addColumnInput ("item", self::coColW9);
 			$this->closeRow();
 
-      $this->openRow();
+			if ($testDocRowPriceSource)
+			{
+				$this->openRow();
+
+				$this->addColumnInput ("quantity", self::coColW2);
+				$this->addColumnInput ("unit", self::coColW1);
+				if ($this->recData['priceSource'] == 1)
+				{
+					$this->addColumnInput ("priceItem", self::coColW2|self::coDisabled);
+					$this->addColumnInput ("priceAll", self::coColW3|DataModel::coSaveOnChange);
+				}
+				else
+				{
+					$this->addColumnInput ("priceItem", self::coColW3);
+					$this->addColumnInput ("priceAll", self::coColW2|self::coDisabled);
+				}
+				if ($ownerRecData && $ownerRecData ['taxPayer'])
+					$this->addColumnInput ("taxCode", self::coColW2|DataModel::coSaveOnChange);
+				$this->addColumnInput ("priceSource", self::coColW2|DataModel::coSaveOnChange);
+
+				$this->closeRow();
+			}
+			else
+			{
+				$this->openRow();
 				$this->addColumnInput ("quantity", self::coColW3);
 				$this->addColumnInput ("unit", self::coColW3);
 				$this->addColumnInput ("priceItem", self::coColW3);
 
 				if ($ownerRecData && $ownerRecData ['taxPayer'] && $ownerRecData ['taxCalc'])
 					$this->addColumnInput ("taxCode", self::coColW3);
-			$this->closeRow();
+				$this->closeRow();
+			}
 
 		$this->closeForm ();
 	}
@@ -39,5 +69,4 @@ class FormStockInRow extends \e10doc\core\libs\FormDocRows
     }
     return parent::columnLabel ($colDef, $options);
   }
-
 }
