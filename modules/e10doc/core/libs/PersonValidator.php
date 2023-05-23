4f9072c90df459b69974edbdc508ea8e032b34b9
David Šebík <sebik@shipard.cz>
2023-05-23 11:19:06 +0200

    Příprava na lepší kontrolu nových Osob

--- modules/e10doc/core/libs/PersonValidator.php
+++ modules/e10doc/core/libs/PersonValidator.php
@@ -13,20 +13,20 @@ class PersonValidator extends Utility
 
   public function batchCheck()
   {
-    $maxOldDate = new \DateTime('1 year ago');
+		$testNewPersons = intval($this->app()->cfgItem ('options.persons.testNewPersons', 0));
+    if (!$testNewPersons)
+      return;
 
     $q[] = 'SELECT * FROM [e10_persons_persons] as persons ';
 		array_push($q, ' WHERE 1');
 		array_push($q, ' AND [docState] = %i', 4000);
     array_push($q, ' AND [company] = %i', 1);
 
-    array_push($q, ' AND EXISTS (SELECT person FROM e10doc_core_heads WHERE persons.ndx = person AND dateAccounting > %d', $maxOldDate,
-        ' AND docType IN %in', ['invno', 'invni', 'cash', 'cashreg'], ')');
-
     array_push($q, ' AND (');
     array_push($q, ' EXISTS (SELECT ndx FROM e10_persons_personsValidity WHERE persons.ndx = person AND [valid] = %i)', 0);
     array_push($q, ' OR NOT EXISTS (SELECT ndx FROM e10_persons_personsValidity WHERE persons.ndx = person)');
     array_push($q, ')');
+
 		array_push($q, ' LIMIT 0, %i', $this->maxCount);
 
 		$rows = $this->db()->query($q);
@@ -38,7 +38,7 @@ class PersonValidator extends Utility
       $pv->setPersonNdx($r['ndx']);
       $pv->checkPerson();
 
-      sleep(5);
+      sleep(1);
 		}
   }
 }
