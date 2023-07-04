65dfa921713d38c7ff18aeafc687a92460d4b58f
David Šebík <sebik@shipard.cz>
2023-07-04 12:29:20 +0200

    V detailu dokladu jsou vidět zdrojové doklady z generování

--- modules/e10doc/gen/libs/DocDetailPartGen.php
+++ modules/e10doc/gen/libs/DocDetailPartGen.php
@@ -8,10 +8,52 @@ namespace e10doc\gen\libs;
  */
 class DocDetailPartGen extends \e10doc\base\libs\DocDetailPart
 {
+  public function doIt_From()
+  {
+    $q = [];
+    array_push($q, 'SELECT requests.cfg, requests.srcType, requests.srcDocument, requests.dstDocument,');
+    array_push($q, ' docHeads.docNumber, docHeads.docState, docHeads.docStateMain, docHeads.docType, docHeads.ndx');
+    array_push($q, ' FROM [e10doc_gen_requests] AS requests');
+    array_push($q, ' LEFT JOIN [e10doc_core_heads] AS docHeads ON requests.srcDocument = docHeads.ndx');
+    array_push($q, ' WHERE 1');
+    array_push($q, ' AND requests.[dstDocument] = %i', $this->recData['ndx']);
+
+    $existedDocs = [];
+    $rows = $this->db()->query($q);
+    foreach ($rows as $r)
+    {
+      $existedDocs[] = $r->toArray();
+    }
+
+    if (!count($existedDocs))
+      return;
+
+
+    $test = [];
+    $test[] = ['text' => 'Vystaveno z dokladu', 'class' => 'h2'];
+
+    foreach ($existedDocs as $doc)
+    {
+      $openButton = [
+        'docAction' => 'edit',
+        'text' => $doc['docNumber'], 'pk' => $doc['srcDocument'], 'table' => 'e10doc.core.heads',
+        'icon' => $this->tableDocsHeads->tableIcon($doc),
+        'type' => 'button',
+        'actionClass' => 'btn btn-primary',
+        'class' => 'pull-right'
+      ];
+      $test [] = $openButton;
+
+      $this->addContent(['type' => 'line', 'pane' => 'e10-pane e10-pane-table', 'line' => $test]);
+    }
+  }
+
   public function doIt()
   {
     if (!$this->gens)
       return;
+
+    $this->doIt_From();
 
     $existedDocs = [];
 
