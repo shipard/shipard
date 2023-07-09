476fc8c5400a363f6c2394ac1084a836ee5b0f90
David Šebík <sebik@shipard.cz>
2023-07-09 19:22:56 +0200

    Opravy v přidávání nových verzí SW

--- modules/services/sw/libs/SWInfoAnalyzer.php
+++ modules/services/sw/libs/SWInfoAnalyzer.php
@@ -353,29 +353,6 @@ class SWInfoAnalyzer extends Utility
 		}
 		elseif ($cnt === 0)
 		{
-			if ($r['swVersionsMode'] == 0 && $this->swVersion !== '' && $this->swVersion !== '-') // auto add versions
-			{
-				$newVersion = [
-					'sw' => $this->swNdx,
-					'versionNumber' => $this->swVersion,
-					'lifeCycle' => 9,
-				];
-
-				/** @var \mac\sw\TableSWVersions */
-				$tableSWVersions = $this->app()->table('mac.sw.swVersions');
-
-				$newVersionNdx = $tableSWVersions->dbInsertRec($newVersion);
-				$newVersion = $tableSWVersions->loadItem($newVersionNdx);
-				$tableSWVersions->checkAfterSave2 ($newVersion);
-				$tableSWVersions->docsLog ($newVersionNdx);
-				$this->addProtocol('check-sw-version', "SW version `{$this->swVersion}` / `{$newVersion['suid']}` added");
-
-				$this->swVersionNdx = $newVersionNdx;
-				$this->swVersionSUID = $newVersion['suid'];
-
-				return;
-			}
-
 			$this->addProtocol('search-sw', "No SW found");
 			$this->error = TRUE;
 			return;
@@ -415,6 +392,29 @@ class SWInfoAnalyzer extends Utility
 		}
 		elseif ($cnt === 0)
 		{
+			if ($r['swVersionsMode'] == 0 && $this->swVersion !== '' && $this->swVersion !== '-') // auto add versions
+			{
+				$newVersion = [
+					'sw' => $this->swNdx,
+					'versionNumber' => $this->swVersion,
+					'lifeCycle' => 9,
+				];
+
+				/** @var \mac\sw\TableSWVersions */
+				$tableSWVersions = $this->app()->table('mac.sw.swVersions');
+
+				$newVersionNdx = $tableSWVersions->dbInsertRec($newVersion);
+				$newVersion = $tableSWVersions->loadItem($newVersionNdx);
+				$tableSWVersions->checkAfterSave2 ($newVersion);
+				$tableSWVersions->docsLog ($newVersionNdx);
+				$this->addProtocol('check-sw-version', "SW version `{$this->swVersion}` / `{$newVersion['suid']}` added");
+
+				$this->swVersionNdx = $newVersionNdx;
+				$this->swVersionSUID = $newVersion['suid'];
+
+				return;
+			}
+
 			$this->addProtocol('search-sw-version', "No SW version found");
 			$this->error = TRUE;
 			return;
