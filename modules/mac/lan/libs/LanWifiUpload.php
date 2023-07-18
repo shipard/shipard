78bb2f629a7ad36e97409121f89ab4bf79314a0a
David Šebík <sebik@shipard.cz>
2023-07-18 18:57:42 +0200

    LAN: první verze přehledu wifi klientů

---
+++ modules/mac/lan/libs/LanWifiUpload.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace mac\lan\libs;
+
+use e10\Utility, e10\json;
+use \Shipard\Utils\Str;
+
+
+/**
+ * class LanWifiUpload
+ */
+class LanWifiUpload extends Utility
+{
+	public $result = ['success' => 0];
+
+	public function run ()
+	{
+		$data = json_decode($this->app()->postData(), TRUE);
+		if (!$data || !isset($data['clients']))
+			return;
+
+
+    $cmsDeviceNdx = intval($data['cmsDevice'] ?? 0);
+    $activeMacsNdxs = [];
+
+		$this->db()->begin();
+
+		// -- clients
+		foreach ($data['clients'] as $client)
+		{
+      $mac = $client['mac'];
+      if ($mac === '')
+        continue;
+
+			$exist = $this->app()->db()->query('SELECT ndx FROM [mac_lan_wifiClients] WHERE [mac] = %s', $mac)->fetch();
+			if ($exist)
+			{ // update
+				$update = [
+          'cmsDevice' => $cmsDeviceNdx,
+          'hostName' => Str::upToLen($client['hostName'] ?? '', 120),
+          'rssi' => intval($client['rssi'] ?? 0),
+          'ssid' => Str::upToLen($client['ssid'] ?? '', 60),
+          'apId' => Str::upToLen($client['apId'] ?? '', 60),
+          'cch' => Str::upToLen($client['cch'] ?? '', 80),
+          'txRate' => Str::upToLen($client['txRate'] ?? '', 80),
+          'rxRate' => Str::upToLen($client['rxRate'] ?? '', 80),
+
+          'updated' => new \DateTime()
+        ];
+				$this->app()->db()->query('UPDATE [mac_lan_wifiClients] SET ', $update, ' WHERE [ndx] = %i', $exist['ndx']);
+        $activeMacsNdxs[] = $exist['ndx'];
+			}
+			else
+			{ // insert
+				$newItem = [
+					'mac' => $mac,
+          'cmsDevice' => $cmsDeviceNdx,
+          'hostName' => Str::upToLen($client['hostName'] ?? '', 120),
+          'rssi' => intval($client['rssi'] ?? 0),
+          'ssid' => Str::upToLen($client['ssid'] ?? '', 60),
+          'apId' => Str::upToLen($client['apId'] ?? '', 60),
+          'cch' => Str::upToLen($client['cch'] ?? '', 80),
+          'txRate' => Str::upToLen($client['txRate'] ?? '', 80),
+          'rxRate' => Str::upToLen($client['rxRate'] ?? '', 80),
+
+					'created' => new \DateTime(),
+          'updated' => new \DateTime()
+				];
+				$this->app()->db()->query('INSERT INTO [mac_lan_wifiClients] ', $newItem);
+        $newNdx = intval ($this->app()->db()->getInsertId ());
+        $activeMacsNdxs[] = $newNdx;
+			}
+		}
+
+    if (count($activeMacsNdxs))
+    {
+      $this->app()->db()->query('UPDATE [mac_lan_wifiClients] SET [inactive] = %i', 1, ' WHERE [ndx] NOT IN %in', $activeMacsNdxs,
+                                ' AND cmsDevice = %i', $cmsDeviceNdx);
+    }
+    else
+    {
+      $this->app()->db()->query('UPDATE [mac_lan_wifiClients] SET [inactive] = %i', 1, ' WHERE cmsDevice = %i', $cmsDeviceNdx);
+    }
+		$this->db()->commit();
+
+		$this->result ['success'] = 1;
+	}
+}
---
+++ modules/mac/lan/tables/wifiClients.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace mac\lan;
+
+use \Shipard\Viewer\TableViewGrid, \Shipard\Viewer\TableViewDetail, \Shipard\Form\TableForm, \Shipard\Table\DbTable, \Shipard\Utils\Utils;
+
+
+/**
+ * class TableWifiClients
+ */
+class TableWifiClients extends DbTable
+{
+	public function __construct ($dbmodel)
+	{
+		parent::__construct ($dbmodel);
+		$this->setName ('mac.lan.wifiClients', 'mac_lan_wifiClients', 'WiFi klienti');
+	}
+
+	public function createHeader ($recData, $options)
+	{
+		$h = parent::createHeader ($recData, $options);
+		$h ['info'][] = ['class' => 'title', 'value' => $recData ['mac']];
+
+		return $h;
+	}
+}
+
+
+/**
+ * class ViewWifiClients
+ */
+class ViewWifiClients extends TableViewGrid
+{
+	var $macs = [];
+	var $macDevicesLabels = [];
+
+	public function init ()
+	{
+		parent::init();
+
+    $this->enableDetailSearch = TRUE;
+    $this->type = 'form';
+
+    $this->fullWidthToolbar = TRUE;
+		$this->gridEditable = TRUE;
+		$this->enableToolbar = TRUE;
+
+    $this->objectSubType = self::vsMain;
+    $this->linesWidth = 80;
+
+		$g = [
+			'mac' => 'MAC',
+      'hostName' => 'Jméno',
+      'ap' => 'AP',
+      'ssid' => 'SSID',
+      'rssi' => 'rssi',
+      'cch' => 'Kanál',
+      'rxtx' => 'RX / TX',
+		];
+
+    $this->setGrid ($g);
+
+    $this->setPanels (self::sptQuery);
+	}
+
+	public function renderRow ($item)
+	{
+		$this->macs[] = $item['mac'];
+
+		$listItem ['pk'] = $item ['ndx'];
+
+		$listItem ['mac'] = $item ['mac'];
+    $listItem ['hostName'] = $item ['hostName'];
+    $listItem ['rssi'] = $item ['rssi'];
+    $listItem ['ssid'] = $item ['ssid'];
+    $listItem ['ap'] = $item ['apId'];
+    $listItem ['cch'] = $item ['cch'];
+
+    $listItem ['rxtx'] = [
+      ['text' => $item ['rxRate'], 'class' => 'e10-small block'],
+      ['text' => $item ['txRate'], 'class' => 'e10-small block'],
+    ];
+
+		$listItem ['icon'] = $this->table->tableIcon ($item);
+
+    if ($item['inactive'])
+      $listItem ['class'] = 'e10-off';
+
+    //$listItem['_options']['cellCss'] = ['subject' => $css];
+
+		return $listItem;
+	}
+
+	function decorateRow (&$item)
+	{
+    /*
+		if (isset ($this->macDevicesLabels [$item ['mac']]))
+		{
+			if (count($this->macDevicesLabels [$item ['mac']]) === 1)
+			{
+				$item['icon'] = $this->macDevicesLabels [$item ['mac']][0]['icon'];
+				unset($this->macDevicesLabels [$item ['mac']][0]['icon']);
+			}
+			$item ['t2'] = $this->macDevicesLabels [$item ['mac']];
+		}
+    */
+	}
+
+	public function selectRows ()
+	{
+		$fts = $this->fullTextSearch ();
+
+		$q [] = 'SELECT [wc].* ';
+		array_push($q, ' FROM [mac_lan_wifiClients] AS [wc]');
+		array_push($q, ' WHERE 1');
+
+		// -- fulltext
+		if ($fts != '')
+		{
+			array_push($q, ' AND (');
+			array_push($q, 'wc.[mac] LIKE %s', '%' . $fts . '%');
+      array_push($q, 'OR wc.[hostName] LIKE %s', '%' . $fts . '%');
+			array_push($q, ')');
+		}
+
+		array_push($q, ' ORDER BY [inactive], [mac], [ndx]');
+		array_push($q, $this->sqlLimit ());
+
+		$this->runQuery ($q);
+	}
+
+	public function selectRows2 ()
+	{
+		if (!count ($this->pks))
+			return;
+	}
+
+	public function createToolbar ()
+	{
+		return [];
+	}
+}
+
+
+/**
+ * class FormWifiClient
+ */
+class FormWifiClient extends TableForm
+{
+	public function renderForm ()
+	{
+		$this->setFlag ('formStyle', 'e10-formStyleSimple');
+		$this->readOnly = TRUE;
+
+		$this->openForm ();
+			$this->addColumnInput ('mac');
+			//$this->addColumnInput ('ports');
+		$this->closeForm ();
+	}
+}
+
+
+/**
+ * class ViewDetailWifiClient
+ */
+class ViewDetailWifiClient extends TableViewDetail
+{
+	public function createDetailContent ()
+	{
+		//$this->addDocumentCard('mac.lan.dc.DocumentCardMac');
+	}
+
+	public function createToolbar ()
+	{
+		return [];
+	}
+}
+
