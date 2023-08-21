66d113759eb67b1cee625b3897e12182e6b01d56
David Šebík <sebik@shipard.cz>
2023-08-21 10:27:53 +0200

    Průběžné práce na novém UI 2

--- src/_deprecated/e10cfg.php
+++ src/_deprecated/e10cfg.php
@@ -6,6 +6,9 @@ namespace E10;
 use \Shipard\Viewer\TableViewPanel;
 use \Shipard\Form\TableForm;
 use \Shipard\Form\FormSidebar;
+use \Shipard\Utils\Json;
+
+
 
 function wcardtest ($pattern, $string)
 {
@@ -55,63 +58,6 @@ function getRelativePath($from, $to)
         }
     }
     return implode('/', $relPath);
-}
-
-
-
-/** JSON.minify()
- *	v0.1 (c) Kyle Simpson
- *  @https://github.com/getify/JSON.minify
- *	MIT License
- */
-
-function json_minify($json) {
-	$tokenizer = "/\"|(\/\*)|(\*\/)|(\/\/)|\n|\r/";
-	$in_string = false;
-	$in_multiline_comment = false;
-	$in_singleline_comment = false;
-	$tmp; $tmp2; $new_str = array(); $ns = 0; $from = 0; $lc; $rc; $lastIndex = 0;
-
-	while (preg_match($tokenizer,$json,$tmp,PREG_OFFSET_CAPTURE,$lastIndex)) {
-		$tmp = $tmp[0];
-		$lastIndex = $tmp[1] + strlen($tmp[0]);
-		$lc = substr($json,0,$lastIndex - strlen($tmp[0]));
-		$rc = substr($json,$lastIndex);
-		if (!$in_multiline_comment && !$in_singleline_comment) {
-			$tmp2 = substr($lc,$from);
-			if (!$in_string) {
-				$tmp2 = preg_replace("/(\n|\r|\s)*/","",$tmp2);
-			}
-			$new_str[] = $tmp2;
-		}
-		$from = $lastIndex;
-
-		if ($tmp[0] == "\"" && !$in_multiline_comment && !$in_singleline_comment) {
-			preg_match("/(\\\\)*$/",$lc,$tmp2);
-			if (!$in_string || !$tmp2 || (strlen($tmp2[0]) % 2) == 0) {	// start of string with ", or unescaped " character found to end string
-				$in_string = !$in_string;
-			}
-			$from--; // include " character in next catch
-			$rc = substr($json,$from);
-		}
-		else if ($tmp[0] == "/*" && !$in_string && !$in_multiline_comment && !$in_singleline_comment) {
-			$in_multiline_comment = true;
-		}
-		else if ($tmp[0] == "*/" && !$in_string && $in_multiline_comment && !$in_singleline_comment) {
-			$in_multiline_comment = false;
-		}
-		else if ($tmp[0] == "//" && !$in_string && !$in_multiline_comment && !$in_singleline_comment) {
-			$in_singleline_comment = true;
-		}
-		else if (($tmp[0] == "\n" || $tmp[0] == "\r") && !$in_string && !$in_multiline_comment && $in_singleline_comment) {
-			$in_singleline_comment = false;
-		}
-		else if (!$in_multiline_comment && !$in_singleline_comment && !(preg_match("/\n|\r|\s/",$tmp[0]))) {
-			$new_str[] = $tmp[0];
-		}
-	}
-	$new_str[] = $rc;
-	return implode("",$new_str);
 }
 
 
@@ -273,7 +219,7 @@ class TblAppOptions extends DbTable
 		return $f;
 	}
 
-	public function getTableView ($viewId, $queryParams = NULL)
+	public function getTableView ($viewId, $queryParams = NULL, $requestParams = NULL)
 	{
 		$v = new ViewAppOptions ($this, $viewId);
 		$v->init ();
@@ -341,7 +287,7 @@ class TblAppOptions extends DbTable
 					$item[$k] = $saveData['recData'][$k];
 			}
 
-			file_put_contents ($fileName, json_encode ($item));
+			file_put_contents ($fileName, Json::lint ($item));
 		}
 
 		$formData->recData = $this->loadItem ($ndx);
