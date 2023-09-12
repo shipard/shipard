fb8c38f6425c026694014717816e5ec5b448e8f3
David Šebík <sebik@shipard.cz>
2023-09-12 15:41:44 +0200

    Aktivační e-maily se odesílají pouze uživatelům s e-mailem

--- modules/e10/users/services.php
+++ modules/e10/users/services.php
@@ -12,10 +12,10 @@ class ModuleServices extends \e10\cli\ModuleServices
 	protected function addUsersFromContacts()
 	{
 		$tableUsers = new \e10\users\TableUsers($this->app());
-		$this->db()->query('DELETE FROM e10_users_users');
+		//$this->db()->query('DELETE FROM e10_users_users');
 
 		$tableRequests = new \e10\users\TableRequests($this->app());
-		$this->db()->query('DELETE FROM e10_users_requests');
+		//$this->db()->query('DELETE FROM e10_users_requests');
 
     $q = [];
     array_push($q, 'SELECT contacts.*,');
@@ -63,6 +63,7 @@ class ModuleServices extends \e10\cli\ModuleServices
 		array_push($q, ' LEFT JOIN e10_users_users AS [users] ON [requests].[user] = [users].ndx');
 		array_push($q, ' WHERE 1');
 		array_push($q, ' AND [users].[docState] = %i', 4000);
+		array_push($q, ' AND [users].[email] != %s', '');
 		array_push($q, ' AND requestState <= %i', 1);
 		array_push($q, ' ORDER BY [requests].ndx');
 
