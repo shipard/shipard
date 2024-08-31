8c47c92e7d3589205c2b2e79a037af05dd201f35
David Šebík <sebik@shipard.cz>
2024-08-31 09:02:34 +0200

    Nové UI: oprava výchozí PWA ikony

--- src/UI/ng/WebManifest.php
+++ src/UI/ng/WebManifest.php
@@ -55,16 +55,26 @@ class WebManifest extends \Shipard\UI\ng\AppPageBlank
 
 		if (substr($pwaIcon, -4, 4) === '.svg')
 		{
-			$wm['icons'][] = ['src' => 'imgs/-i192/'.$pwaIcon, 'sizes' => '192x192', 'type' => 'image/png'];
-			$wm['icons'][] = ['src' => 'imgs/-i512/'.$pwaIcon, 'sizes' => '512x512', 'type' => 'image/png'];
-			$wm['icons'][] = ['src' => 'imgs/-i1024/'.$pwaIcon, 'sizes' => '1024x1024', 'type' => 'image/png'];
-			$wm['icons'][] = ['src' => 'imgs/-i1980/'.$pwaIcon, 'sizes' => '1980x1980', 'type' => 'image/png'];
-			$wm['icons'][] = ['src' => $pwaIcon, 'type' => 'image/svg+xml'];
+			if (str_starts_with($pwaIcon, 'att/'))
+			{
+				$wm['icons'][] = ['src' => 'imgs/-i192/'.$pwaIcon, 'sizes' => '192x192', 'type' => 'image/png'];
+				$wm['icons'][] = ['src' => 'imgs/-i512/'.$pwaIcon, 'sizes' => '512x512', 'type' => 'image/png'];
+				$wm['icons'][] = ['src' => 'imgs/-i1024/'.$pwaIcon, 'sizes' => '1024x1024', 'type' => 'image/png'];
+				$wm['icons'][] = ['src' => 'imgs/-i1980/'.$pwaIcon, 'sizes' => '1980x1980', 'type' => 'image/png'];
+				$wm['icons'][] = ['src' => $pwaIcon, 'type' => 'image/svg+xml'];
+			}
+			else
+				$wm['icons'][] = ['src' => $pwaIcon, 'type' => 'image/svg+xml'];
 		}
 		else
 		{
-			$wm['icons'][] = ['src' => 'imgs/-i192/'.$pwaIcon, 'sizes' => '192x192'];
-			$wm['icons'][] = ['src' => 'imgs/-i512/'.$pwaIcon, 'sizes' => '512x512'];
+			if (str_starts_with($pwaIcon, 'att/'))
+			{
+				$wm['icons'][] = ['src' => 'imgs/-i192/'.$pwaIcon, 'sizes' => '192x192'];
+				$wm['icons'][] = ['src' => 'imgs/-i512/'.$pwaIcon, 'sizes' => '512x512'];
+			}
+			else
+				$wm['icons'][] = ['src' => $pwaIcon];
 		}
 
 		$code = Json::lint($wm);
