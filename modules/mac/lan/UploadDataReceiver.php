5de455d177640f84b25fd5030588b352daf1d238
David Šebík <sebik@shipard.cz>
2025-01-25 08:38:33 +0000

    IoT: informace o IoT zařízeních

--- modules/mac/lan/UploadDataReceiver.php
+++ modules/mac/lan/UploadDataReceiver.php
@@ -76,18 +76,84 @@ class UploadDataReceiver extends Utility
 		return 'OK';
 	}
 
+	protected function doShnIbInfo ()
+	{
+		$deviceRecData = NULL;
+		if (isset($this->data['devId']))
+			$deviceRecData = $this->db()->query('SELECT * FROM [mac_iot_devices] WHERE [friendlyId] = %s', $this->data['devId'])->fetch();
+		if (!$deviceRecData && intval($this->data['devNdx'] ?? 0))
+			$deviceRecData = $this->db()->query('SELECT * FROM [mac_iot_devices] WHERE [ndx] = %i', intval($this->data['devNdx']))->fetch();
+
+		if (!$deviceRecData)
+			return FALSE;
+
+		$deviceInfoNdx = 0;
+		$deviceInfoRecData = $this->db()->query('SELECT * FROM [mac_iot_devicesInfo] WHERE [device] = %i', $deviceRecData['ndx'])->fetch();
+		if (!$deviceInfoRecData)
+		{
+			$info = ['device' => $deviceRecData['ndx'], 'dateCreate' => new \DateTime()];
+			$this->db()->query('INSERT INTO [mac_iot_devicesInfo] ', $info);
+			$deviceInfoNdx = $this->db()->getInsertId();
+		}
+		else
+			$deviceInfoNdx = $deviceInfoRecData['ndx'];
+
+		$update = [
+			'dateUpdate' => new \DateTime(),
+		];
+
+		if (isset($this->data['items']['verFW']))
+			$update['fwVersion'] = $this->data['items']['verFW'];
+		if (isset($this->data['items']['verOS']))
+			$update['osVersion'] = $this->data['items']['verOS'];
+		if (isset($this->data['items']['devType']))
+			$update['devType'] = $this->data['items']['devType'];
+
+		if (intval($this->data['uptime'] ?? 0))
+			$update['uptime'] = intval($this->data['uptime'] ?? 0);
+
+		if (isset($this->data['battery'])) // zigbee devices
+			$update['pwrBatteryLevel'] = $this->data['battery'];
+		if (isset($this->data['linkquality'])) // zigbee devices
+			$update['signalLevel'] = intval($this->data['linkquality']);
+
+		if (isset($this->data['pwr-batt-perc']))
+			$update['pwrBatteryLevel'] = $this->data['pwr-batt-perc'];
+		if (isset($this->data['pwr-batt-voltage']))
+			$update['pwrBatteryVoltage'] = $this->data['pwr-batt-voltage'];
+
+		$this->db()->query('UPDATE [mac_iot_devicesInfo] SET ', $update, ' WHERE [ndx] = %i', $deviceInfoNdx);
+
+		return 'OK';
+	}
+
 	public function run ()
 	{
-		if (!isset($this->data['type']))
+		if (isset($this->data['infoType']))
+		{
+			if ($this->data['infoType'] === 'shn-ib-info')
+				return $this->doShnIbInfo ();
+
 			return 'FALSE';
+		}
 
-		$this->tableUnknowns = $this->app->table ('mac.lan.unknowns');
+		if (isset($this->data['type']))
+		{
+			if ($this->data['type'] === 'e10-nl-unkip')
+			{
+				$this->tableUnknowns = $this->app->table ('mac.lan.unknowns');
+				return $this->doUnknowns ();
+			}
 
-		if ($this->data['type'] === 'e10-nl-unkip')
-			return $this->doUnknowns ();
+			if ($this->data['type'] === 'e10-nl-snmp')
+				return $this->doInfo ();
 
-		if ($this->data['type'] === 'e10-nl-snmp')
-			return $this->doInfo ();
+
+			if ($this->data['type'] === 'shn-ib-info')
+				return $this->doShnIbInfo ();
+
+			return 'FALSE';
+		}
 
 		return 'FALSE';
 	}
