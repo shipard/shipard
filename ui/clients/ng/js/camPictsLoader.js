945b27ca865295dde225d096610a73918408ec17
David Šebík <sebik@shipard.cz>
2025-04-15 20:28:04 +0000

    Opravy nových kamer v novém UI

--- ui/clients/ng/js/camPictsLoader.js
+++ ui/clients/ng/js/camPictsLoader.js
@@ -1,7 +1,5 @@
 class ShipardCamsPictsLoader
 {
-  camerasTimer = null;
-
   init()
   {
     this.reloadImages();
@@ -9,49 +7,19 @@ class ShipardCamsPictsLoader
 
   reloadImages()
   {
-    //console.log('reload-images', uiData['iotCamServers']);
-    if (this.camerasTimer) {
-      clearTimeout(this.camerasTimer);
-    }
-
-    for (let serverNdx in uiData['iotCamServers'])
-    {
-      const urlPath = uiData['iotCamServers'][serverNdx]['camUrl'] + "campicts";
-      shc.server.get (urlPath,
-        function (data) {
-          this.setPictures(serverNdx, data);
-        }.bind(this),
-        function (data) {
-          console.log("--load-error--");
-        }.bind(this),
-        1
-      );
-    }
+    this.setPictures();
   }
 
-  setPictures(serverNdx, data)
+  setPictures()
   {
-    //console.log('set-pictures', data);
-    const server = uiData['iotCamServers'][serverNdx];
-    for (let camNdx in data)
+    for (let camId in uiData['iotCamPictures'])
     {
-      if ( data[camNdx]['image'] === false)
-      {
-        continue;
-      }
-      let camId = 'CMP' + camNdx;
-      if (uiData['iotCamPictures'][camId] === undefined)
-        continue;
-
-      let pictUrl = '';
       let ids = uiData['iotCamPictures'][camId]['elms'];
       for (var key in ids)
       {
         let camPictElement = document.getElementById(ids[key]);
         if (!camPictElement)
         {
-          //console.log("Invalid element", key);
-          //uiData['iotCamPictures'][camId]['elms'][key];
           continue;
         }
 
@@ -59,24 +27,12 @@ class ShipardCamsPictsLoader
 
         if (pictStyle === 'video')
         {
-          let videoElement = camPictElement.querySelector('video');
-
           const played = parseInt(camPictElement.getAttribute('data-stream-started'));
           if (!played)
           {
             this.startVideoGO2RTC(camPictElement);
             camPictElement.setAttribute('data-stream-started', '1');
           }
-        }
-        else
-        {
-          if (pictStyle === 'full')
-            pictUrl = server['camUrl'] + 'imgs/' + camNdx + '/' + data[camNdx]['image'];
-          else
-            pictUrl = server['camUrl'] + 'imgs/-w960/-q70/' + camNdx + '/' + data[camNdx]['image'];
-
-          let imgElement = camPictElement.querySelector('img');
-          imgElement.src = pictUrl;
         }
       }
     }
--- www-root/.ui/ng/js/client.js
+++ www-root/.ui/ng/js/client.js
@@ -71,27 +71,17 @@ ws=webSocketServers[serverIndex];}publish(serverIndex,topic,payload){var
 ws=webSocketServers[serverIndex];if(!ws.hasOwnProperty('mqttClient'))return 0;let
 message=new
 Paho.MQTT.Message(payload);message.destinationName=topic;ws.mqttClient.send(message);return 1;}applyUIData(responseUIData){console.log("ShipardMqtt - apply uiData: ",responseUIData);}}class
-ShipardCamsPictsLoader{camerasTimer=null;init(){this.reloadImages();}reloadImages(){if(this.camerasTimer){clearTimeout(this.camerasTimer);}for(let
-serverNdx
+ShipardCamsPictsLoader{init(){this.reloadImages();}reloadImages(){this.setPictures();}setPictures(){for(let
+camId
 in
-uiData['iotCamServers']){const
-urlPath=uiData['iotCamServers'][serverNdx]['camUrl']+"campicts";shc.server.get(urlPath,function(data){this.setPictures(serverNdx,data);}.bind(this),function(data){console.log("--load-error--");}.bind(this),1);}}setPictures(serverNdx,data){const
-server=uiData['iotCamServers'][serverNdx];for(let
-camNdx
-in
-data){if(data[camNdx]['image']===false){continue;}let
-camId='CMP'+camNdx;if(uiData['iotCamPictures'][camId]===undefined)continue;let
-pictUrl='';let
+uiData['iotCamPictures']){let
 ids=uiData['iotCamPictures'][camId]['elms'];for(var
 key
 in
 ids){let
 camPictElement=document.getElementById(ids[key]);if(!camPictElement){continue;}let
-pictStyle=camPictElement.getAttribute('data-pict-style');if(pictStyle==='video'){let
-videoElement=camPictElement.querySelector('video');const
-played=parseInt(camPictElement.getAttribute('data-stream-started'));if(!played){this.startVideoGO2RTC(camPictElement);camPictElement.setAttribute('data-stream-started','1');}}else{if(pictStyle==='full')pictUrl=server['camUrl']+'imgs/'+camNdx+'/'+data[camNdx]['image'];else
-pictUrl=server['camUrl']+'imgs/-w960/-q70/'+camNdx+'/'+data[camNdx]['image'];let
-imgElement=camPictElement.querySelector('img');imgElement.src=pictUrl;}}}}startVideoGO2RTC(videoEl){let
+pictStyle=camPictElement.getAttribute('data-pict-style');if(pictStyle==='video'){const
+played=parseInt(camPictElement.getAttribute('data-stream-started'));if(!played){this.startVideoGO2RTC(camPictElement);camPictElement.setAttribute('data-stream-started','1');}}}}}startVideoGO2RTC(videoEl){let
 streamUrl=videoEl.getAttribute('data-stream-url');let
 streamId=videoEl.getAttribute('data-stream-id');const
 video=document.createElement('video-stream');video.src=new
