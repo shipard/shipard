35f0c303fc7bd4fa3773cc81845a6e560f3a8c09
David Šebík <sebik@shipard.cz>
2025-06-28 14:50:10 +0000

    IoT: HTTP API pro IoT a E-cedulky

---
+++ modules/mac/iot/libs/ESignsApi.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace mac\iot\libs;
+use \Shipard\Utils\Json;
+
+
+/**
+ * class ESignsApi
+ */
+class ESignsApi extends \mac\iot\libs\IoTApi
+{
+  var $esignRecData = NULL;
+  var $esignImageRecData = NULL;
+
+  protected function doOperation()
+  {
+    switch($this->operation)
+    {
+      case 'getESignImageInfo':
+      case 'getESignImageVersion':
+      case 'getESignImagePreviewEInk':
+      case 'getESignImagePreviewDisplay':
+      case 'getESignImageEInk':
+        $this->opESignImageInfo();
+        return;
+    }
+
+    parent::doOperation();
+  }
+
+  protected function opESignImageInfo()
+  {
+    $e = new \mac\iot\libs\ESignImageEngine($this->app());
+    $e->setESign(intval($this->esignRecData['ndx']));
+    $e->doIt();
+
+    $esignImageRecData = $this->app()->loadItem(intval($this->esignRecData['ndx']), 'mac.iot.esignsImgs');
+		if (!$esignImageRecData)
+    {
+      $this->result['msg'] = 'ESign image for `'.$this->esignRecData['idName'].'` not found';
+      return;
+    }
+
+    if ($this->operation === 'getESignImageVersion')
+    {
+      $this->resultMimeType = 'text/plain';
+      $this->resultData = strval($esignImageRecData['version']);
+      return;
+    }
+    elseif ($this->operation === 'getESignImageInfo')
+      $this->resultData = Json::lint($esignImageRecData);
+    elseif ($this->operation === 'getESignImagePreviewEInk')
+    {
+      $this->resultSrcFileName = __APP_DIR__.'/tmp/'.basename($esignImageRecData['imagePreviewURL']);
+      $this->resultMimeType = 'image/png';
+      $this->resultSaveFileName = 'esign-image-preview-eink.png';
+    }
+    elseif ($this->operation === 'getESignImagePreviewDisplay')
+    {
+      $this->resultSrcFileName = __APP_DIR__.'/tmp/'.basename($esignImageRecData['imagePreviewURL']);
+      $this->resultMimeType = 'image/png';
+      $this->resultSaveFileName = 'esign-image-preview-display.png';
+    }
+    elseif ($this->operation === 'getESignImageEInk')
+    {
+      $this->resultSrcFileName = __APP_DIR__.'/tmp/'.basename($esignImageRecData['imageEinkURL']);
+      $this->resultMimeType = 'application/octet-stream';
+      $this->resultSaveFileName = 'esign-image-eink.sbef';
+    }
+  }
+
+  protected function detectParams()
+  {
+    if (!parent::detectParams())
+      return FALSE;
+
+    $esignId = $this->app->requestPath(4);
+    if ($esignId === '')
+    {
+      $this->result['msg'] = 'Missing ESign ID';
+      return FALSE;
+    }
+
+		$esignRecData = $this->db()->query('SELECT * FROM [mac_iot_esigns] WHERE',
+                                       ' [iotDevice] = %i', intval($this->deviceRecData['ndx']),
+                                       ' AND [idName] = %s', $esignId,
+                                      )->fetch();
+		if (!$esignRecData)
+    {
+      $this->result['msg'] = 'ESign for device `'.$this->deviceRecData['friendlyId'].'` not found';
+      return FALSE;
+    }
+    $this->esignRecData = $esignRecData->toArray();
+
+    return TRUE;
+  }
+}
---
+++ modules/mac/iot/libs/IoTApi.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace mac\iot\libs;
+use \Shipard\Utils\Json, \Shipard\Base\Utility, \Shipard\Application\Response;
+
+
+/**
+ * class IoTApi
+ */
+class IoTApi extends Utility
+{
+  var $deviceUID = '';
+  var $deviceMAC = '';
+  var $deviceRecData = NULL;
+
+  var $operation = '';
+
+	var $result = [];
+  var $resultData = '';
+  var $resultSrcFileName = '';
+  var $resultSaveFileName = '';
+  var $resultMimeType = 'application/json';
+
+	public function init()
+	{
+	}
+
+  protected function detectParams()
+  {
+    // -- deviceId
+    $devId = $this->app->requestPath(2);
+    if (substr($devId, 0, 4) === 'mac:')
+    {
+      $this->deviceMAC = substr($devId, 4);
+      if ($this->deviceMAC === '')
+      {
+        $this->result['msg'] = 'Missing device MAC';
+        return FALSE;
+      }
+      $drd = $this->db()->query('SELECT * FROM [mac_iot_devices] WHERE [hwId] = %s', $this->deviceMAC)->fetch();
+      if (!$drd)
+      {
+        $this->result['msg'] = 'Device with MAC `'.$this->deviceMAC.'` not found';
+        return FALSE;
+      }
+      $this->deviceRecData = $drd->toArray();
+    }
+    else
+    {
+      $this->deviceUID = $devId;
+      if ($this->deviceUID === '')
+      {
+        $this->result['msg'] = 'Missing device UID';
+        return FALSE;
+      }
+      $drd = $this->db()->query('SELECT * FROM [mac_iot_devices] WHERE [uid] = %s', $this->deviceUID)->fetch();
+      if (!$drd)
+      {
+        $this->result['msg'] = 'Device with UID `'.$this->deviceUID.'` not found';
+        return FALSE;
+      }
+      $this->deviceRecData = $drd->toArray();
+    }
+
+    // -- operation
+    $this->operation = $this->app->requestPath(3);
+
+    return TRUE;
+  }
+
+  protected function doOperation()
+  {
+    switch($this->operation)
+    {
+      case 'getDeviceCfg':
+        $this->opDeviceCfg();
+        break;
+      default:
+        $this->result['msg'] = 'Unknown operation `'.$this->operation.'`';
+        return;
+    }
+  }
+
+  protected function opDeviceCfg()
+  {
+		$q[] = 'SELECT * FROM [mac_iot_devicesCfg]';
+		array_push($q, ' WHERE 1');
+		array_push($q, ' AND [iotDevice] = %i', $this->deviceRecData['ndx']);
+		array_push($q, ' ORDER BY [ndx]');
+
+		$rows = $this->db()->query($q);
+		foreach ($rows as $r)
+		{
+      $iotBoxCfg = Json::decode($r['cfgData']);
+      $this->resultData = Json::lint($iotBoxCfg['iotBoxCfg']);
+      return;
+    }
+  }
+
+	public function run ()
+	{
+    if (!$this->detectParams())
+    {
+      $this->result['error'] = 'Invalid parameters';
+      $response = new Response ($this->app);
+      $response->setMimeType('application/json');
+      $response->add ('result', $this->result);
+      $response->setStatus(404);
+      return $response;
+    }
+
+		$this->doOperation();
+
+		$response = new Response ($this->app);
+    if ($this->resultSrcFileName !== '')
+    {
+      $response->setFile($this->resultSrcFileName, $this->resultMimeType, $this->resultSaveFileName);
+    }
+    elseif ($this->resultData !== '')
+    {
+      $response->setMimeType($this->resultMimeType);
+      $response->setRawData($this->resultData);
+      $response->setStatus(200);
+    }
+    else
+    {
+      $response->setMimeType('application/json');
+      $response->add ('result', $this->result);
+      $response->setStatus(404);
+    }
+
+		return $response;
+	}
+}
